e4de5099b9173c51e7432135c6e40642f7d4574a
Rick_Alves <henryworkdev.ti@gmail.com>
2022-02-21 02:07:46 -0300

    modal adjustment

--- src/components/New.tsx
+++ src/components/New.tsx
@@ -3,27 +3,27 @@ import InputField from "./InputField";
 export default function New(props){
     return (
         <section id="modal" className={`
-             flex items-center justify-center 
-             pointer-events-none z-30 opacity-0
-             transition-all duration-300
+             pointer-events-none opacity-0
+             transition-all duration-300 z-20
+             flex justify-center
         `}
         >
-            
             <div className={`
                 bg-slate-500 fixed opacity-60
                 left-0 top-0 w-full h-full
-            `}>
-             </div>
+            `}></div>
+
                 <div className={`
-                    flex flex-col gap-2 bg-white p-4 rounded-md fixed
-                    items-center w-1/3 top-1/4
+                    flex flex-col gap-2 bg-white p-5 rounded-md fixed
+                    w-11/12 md:w-8/12 lg:w-2/4 xl:w-1/3
+                    top-1/4 
                 `}>
-                    <h2 className="text-2xl font-semibold">Cadastrar Novo</h2>
-                    <hr className="border w-96"/>
-                    <InputField label="Matricula" type="number" className="w-96" />
-                    <InputField label="Nome" type="text" className="w-96" />
-                    <InputField label="Idade" type="number" className="w-96" />
-                    <div className="flex justify-end w-full px-10 py-2 gap-2">
+                    <h2 className="text-2xl font-semibold text-center">Cadastrar Novo</h2>
+                    <hr className="border w-full lg:w-96 mx-auto"/>
+                    <InputField label="Matricula" type="number" className="w-full" />
+                    <InputField label="Nome" type="text" className=" w-full" />
+                    <InputField label="Idade" type="number" className="w-full" />
+                    <div className="flex justify-end w-full py-2 gap-2">
                         <button className={`
                             border rounded px-3 py-1 cursor-pointer
                             bg-gradient-to-b from-cyan-500 to-blue-500
